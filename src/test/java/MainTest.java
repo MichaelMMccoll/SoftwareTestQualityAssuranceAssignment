4df4d7e9d743bcb2c8d672046dd286daada87421
MichaelMMccoll <michaelmccoll@gmail.com>
2024-11-29 15:36:39 +0000

    Added example main test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,45 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+
 class MainTest {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        // Redirect System.out to a ByteArrayOutputStream
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void restoreSystemOut() {
+        // Restore the original System.out after each test
+        System.setOut(originalOut);
+    }
+
     @Test
     void main() {
-
+    //Arrange
+    String[] args = {};
+    String input = "3";
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
+    //Act
+        Main.main(args);
+    //Assert
+    var aa = Arrays.stream(outputStream.toString().split("\n")).toList();
+        Assertions.assertTrue(outputStream.toString().contains("Exiting"));
     }
+
+
 
 
 }
--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -43,18 +43,18 @@ class UtilsTest {
 
     //Test crashes when no Recycling center is given
     //This should fail
-//    @Test
-//    @DisplayName("Finding optimal center with there are 0 types")
-//    void findOptimalCentre_When_C_0Centers() {
-//        //Arrange
-//        var recyclingList = new ArrayList<Recycling>();
-//        var aa = new Historic(Location.C, 1251.0);
-//        var errorMessage = "";
-//        //Act
-//        util.findOptimalCentre(aa, recyclingList);
-//        //Assert
-//        //assertThrows(NoSuchElementException.class, () -> util.findOptimalCentre(aa,recyclingList));
-//    }
+    @Test
+    @DisplayName("Finding optimal center with there are 0 types")
+    void findOptimalCentre_When_C_0Centers() {
+        //Arrange
+        var recyclingList = new ArrayList<Recycling>();
+        var aa = new Historic(Location.C, 1251.0);
+        var errorMessage = "";
+        //Act
+        util.findOptimalCentre(aa, recyclingList);
+        //Assert
+        //assertThrows(NoSuchElementException.class, () -> util.findOptimalCentre(aa,recyclingList));
+    }
 
     @ParameterizedTest
     @DisplayName("Calculates travel duration")
